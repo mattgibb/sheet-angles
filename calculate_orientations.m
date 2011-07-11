87ae63f278b76e2f2d5458b25965b5a6a669552a
Matt Gibb <powermoveguru@gmail.com>
2011-07-11 18:03:23 +0100

    Added Rat28 slices.

--- calculate_orientations.m
+++ calculate_orientations.m
@@ -1,16 +1,28 @@
 % files = dir('H:\cygwin\home\matthew.g\images\Rat24\HiRes\originals\');
+% files = {
+%     'Rat24_0396.bmp'
+%     'Rat24_0414.bmp'
+%     'Rat24_0435.bmp'
+%     'Rat24_0455.bmp'
+%     'Rat24_0475.bmp'
+%     'Rat24_0495.bmp'
+%     'Rat24_0516.bmp'
+%     'Rat24_0535.bmp'
+%     'Rat24_0556.bmp'
+%     'Rat24_0576.bmp'
+%     'Rat24_0595.bmp'
+%     };
+
 files = {
-    'Rat24_0396.bmp'
-    'Rat24_0414.bmp'
-    'Rat24_0435.bmp'
-    'Rat24_0455.bmp'
-    'Rat24_0475.bmp'
-    'Rat24_0495.bmp'
-    'Rat24_0516.bmp'
-    'Rat24_0535.bmp'
-    'Rat24_0556.bmp'
-    'Rat24_0576.bmp'
-    'Rat24_0595.bmp'
+    '0391.bmp'
+    '0411.bmp'
+    '0431.bmp'
+    '0451.bmp'
+    '0471.bmp'
+    '0491.bmp'
+    '0511.bmp'  
+    '0531.bmp'
+    '0550.bmp'
     };
 
 for file = 1:length(files)
