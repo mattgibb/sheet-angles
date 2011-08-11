0f711dae0596a8440fd4a12be87e1a934126d2ad
Matt Gibb <powermoveguru@gmail.com>
2011-08-11 12:21:55 +0100

    Refactored angles name as not Gaussian smoothed.

--- angle_bins.m
+++ angle_bins.m
@@ -1,10 +1,11 @@
-function [angles_mean,angles_std] = angle_bins(anglesg, e)
+function [angles_mean,angles_std] = angle_bins(angles, e)
 %ANGLE_BINS Mean and standard dev distribution of angles against e
 er=e;
 er(e>0)=round(er(e>0)*99+1);
 e_avg=zeros(1,100);
 for v=1:100
-    angles_mean(v)=mean(anglesg(er(:)==v));
-    angles_std(v)=std(anglesg(er(:)==v));
+    
+    angles_mean(v)=mean(angles(er(:)==v));
+    angles_std(v)=std(angles(er(:)==v));
 end
 end
