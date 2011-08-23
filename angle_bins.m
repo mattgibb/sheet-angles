4974d8ae020116ec7226397d560fa27fa8fa1469
Matt Gibb <powermoveguru@gmail.com>
2011-08-23 18:26:43 +0100

    Refactored naming.

--- angle_bins.m
+++ angle_bins.m
@@ -2,7 +2,6 @@ function [angles_mean,angles_std] = angle_bins(angles, e)
 %ANGLE_BINS Mean and standard dev distribution of angles against e
 er=e;
 er(e>0)=round(er(e>0)*99+1);
-e_avg=zeros(1,100);
 for v=1:100
     
     angles_mean(v)=mean(angles(er(:)==v));
--- calculate_orientations_in_roi.m
+++ calculate_orientations_in_roi.m
@@ -1,6 +1,6 @@
-function calculate_orientations_in_roi(data_set, image_name, region_name)
+function calculate_orientations_in_roi(data_set, slice_name, region_name)
 fprintf('Reading original file\n')
-im=imread(['H:\cygwin\home\matthew.g\images\' data_set '\HiRes\downsamples_1\' image_name]);
+im=imread(['H:\cygwin\home\matthew.g\images\' data_set '\HiRes\downsamples_1\' slice_name '.bmp']);
 f1=figure;
 imagesc(im)
 axis equal; axis tight
@@ -17,71 +17,69 @@ colormap gray; axis equal; axis tight;
 %Calculate mask
 fprintf('Calculating initial mask\n');
 thres1=250;
-mask=zeros(size(im2));
-mask(im2<thres1)=1;
+threshold_image=zeros(size(im2));
+threshold_image(im2<thres1)=1;
 subplot(1,2,1);
-imagesc(mask);
+imagesc(threshold_image);
 title('Mask before opening and closing');
 se = strel('disk',7);
-mask = imclose(mask,se);
-mask = imopen(mask,se);
+threshold_image = imclose(threshold_image,se);
+threshold_image = imopen(threshold_image,se);
 subplot(1,2,2);
-imagesc(mask);
+imagesc(threshold_image);
 title('Mask after opening and closing');
-%pause; %Check here whether the threshold thres1 is fine.
 
-mask2=bwlabel(mask);
+tissue_label_image=bwlabel(threshold_image);
 f1=figure;
-imagesc(mask2)
+imagesc(tissue_label_image)
 impixelinfo
 f2=figure;
-imagesc(mask2)
+imagesc(tissue_label_image)
 title('Select all regions corresponding to tissue (check the previous figure for values)');
-mask2=uint8(bwselect);
+tissue_label_image=uint8(bwselect);
 close(f1);
 close(f2);
 
 
 %Eliminate internal "holes"
-mask3=bwlabel(~mask2);
+non_tissue_label_image=bwlabel(~tissue_label_image);
 f1=figure;
-%subplot(1,2,1)
-imagesc(mask3)
+imagesc(non_tissue_label_image)
 impixelinfo
 f2=figure;
-imagesc(mask3)
+imagesc(non_tissue_label_image)
 title('Select all regions corresponding to background');
-mask3=uint8(bwselect);
+background_cavity_image=uint8(bwselect);
 title('Select all regions corresponding to cavity');
-mask4=uint8(bwselect);
+cavity_image=uint8(bwselect);
 close(f1);
 close(f2);
-mask3(mask4==1)=2;
+background_cavity_image(cavity_image==1)=2;
 
-
-orient_test=calculate_orientation_ST_2D(im2,3,11, ~mask3);
+orient_test=calculate_orientation_ST_2D(im2,3,11, ~background_cavity_image);
 
 %Select the main orientation of the tissue
 f3 = figure;
-imagesc(mask3)
+imagesc(background_cavity_image)
 title('Select two point to mark the main orientation of the structure')
 [x,y] = ginput(2);
 close(f3);
-%ref_vector=[y(2)-y(1) x(1)-x(2)];
+
+% calculate reference angle
 ref_vector=[x(2)-x(1) y(2)-y(1)];
-ref_vector=ref_vector/norm(ref_vector);
 ref_angle=atan(ref_vector(2)/ref_vector(1));
+
 orient_angle=squeeze(atan(orient_test(2,:,:)./orient_test(1,:,:)));
 angles = orient_angle - ref_angle;
 angles(angles < -pi/2) = angles(angles < -pi/2) + pi;
 angles(angles > pi/2) = angles(angles > pi/2) - pi;
 
-angles(mask3>0)=0;
+angles(background_cavity_image>0)=0;
 
 %Select only clefts
 cleft_thres = 180;  %Select the best threshold for clefts
 clefts=zeros(size(im2));
-clefts(im2>cleft_thres & mask3==0)=1;
+clefts(im2>cleft_thres & ~background_cavity_image)=1;
 
 % Get rid of really large holes
 figure
@@ -95,21 +93,23 @@ subplot(1,2,2);
 imagesc(clefts)
 title('After opening')
 
-anglesg(clefts==0)=0;
+angles(clefts==0)=0;
 figure
 imagesc(angles)
 title('Gaussian-smoothed angles in clefts')
 
-dist_epi=bwdist(mask3==2);
-dist_endo=bwdist(mask3==1);
+dist_epi=bwdist(background_cavity_image==2);
+dist_endo=bwdist(background_cavity_image==1);
 e=dist_endo./(dist_endo+dist_epi);
-e(clefts==0)=0;
+e(~clefts)=0;
 figure
 imagesc(e)
 title('e parameter in clefts')
 [angles_mean,angles_std] = angle_bins(angles, e);
 plot_angle_distribution(angles_mean,angles_std);
-mat_name = [image_name '_' region_name '.mat'];
+
+% save results
+mat_name = [slice_name '.bmp_' region_name '.mat'];
 mat_path = ['H:\cygwin\home\matthew.g\orientations\' data_set '\' mat_name ];
 save(mat_path,'angles','e','ref_angle','rect');
 end
